413344368b18bf062ebee50809a5fa1b5cf01698
Nickrhys <nickrhys@hotmail.com>
2014-09-30 19:16:08 +0100

    can handle password input errors. tested

--- server.rb
+++ server.rb
@@ -30,17 +30,23 @@ post '/peeps' do
 end
 
 get '/users/new' do
+	@user = User.new
 	erb :"users/new"
 end
 
 post '/users' do 
-	user = User.create(:email => params[:email],
+	@user = User.new(:email => params[:email],
 				:password => params[:password], 
 				:password_confirmation => params[:password_confirmation],
 				:username => params[:username],
 				:name => params[:name])
-	session[:user_id] = user.id
-	redirect to('/')
+	if @user.save
+		session[:user_id] = @user.id
+		redirect to('/')
+	else
+		flash[:notice] = "Sorry, your passwords don't match"
+		erb :"users/new"
+	end
 end
 
 get '/sessions/new' do 
@@ -48,8 +54,6 @@ get '/sessions/new' do
 end
 
 post'/sessions' do
-
-
 	email, password = params[:email], params[:password]
 	user = User.authenticate(email, password)
 	if user
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -6,13 +6,17 @@ include SessionHelpers
 feature "user signs up" do
 	
 	scenario "when being logged out" do
-		expect{ sign_up }.to change(User, :count).by(1)
+		lambda { sign_up }.should change(User, :count).by(1)
 		expect(page).to have_content("Welcome, Alice")
 		expect(User.first.email).to eq("alice@example.com")
 	end
-end
 
-	#scenario "with a password thats "
+	scenario "with a password that doesn't match" do 
+		expect{ sign_up('a@a.com', 'pass', 'wrong')}.to change(User, :count).by(0)
+		expect(current_path).to eq('/users')
+		expect(page).to have_content("Sorry, your passwords don't match")
+	end
+end
 
 feature "User signs in" do
 
