521b27056a461c9db59a4da3483ed383dd83e468
Nickrhys <nickrhys@hotmail.com>
2014-10-01 18:24:40 +0100

    added unique username and email. passed tests. refactored a little

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -6,7 +6,7 @@ include SessionHelpers
 feature "user signs up" do
 	
 	scenario "when being logged out" do
-		lambda { sign_up }.should change(User, :count).by(1)
+		expect { sign_up }.to change(User, :count).by(1)
 		expect(page).to have_content("Welcome, Alice")
 		expect(User.first.email).to eq("alice@example.com")
 	end
@@ -16,7 +16,19 @@ feature "user signs up" do
 		expect(current_path).to eq('/users')
 		expect(page).to have_content("Sorry, your passwords don't match")
 	end
-end
+
+	scenario "with a unique username" do  
+			expect{ sign_up }.to change(User, :count).by(1)
+			expect{ sign_up }.to change(User, :count).by(0)
+			expect(page).to have_content("This username is already taken")
+		end
+
+		scenario "with a unique email" do  
+			expect{ sign_up }.to change(User, :count).by(1)
+			expect{ sign_up }.to change(User, :count).by(0)
+			expect(page).to have_content("This email is already taken")
+		end		
+	end
 
 feature "User signs in" do
 
