84abe20a5033f4896ef52b16d2b333e73396aeb5
Nickrhys <nickrhys@hotmail.com>
2014-10-28 17:35:31 +0000

    added header and logo

--- app/server.rb
+++ app/server.rb
@@ -7,7 +7,7 @@ require_relative 'models/user'
 require_relative 'helpers/application'
 require_relative 'data_mapper_setup'
 
-require_relative 'controllers/application.rb'
+require_relative 'controllers/application'
 require_relative 'controllers/peeps'
 require_relative 'controllers/sessions'
 require_relative 'controllers/users'
@@ -18,6 +18,7 @@ use Rack::Flash
 use Rack::MethodOverride
 set :partial_template_engine, :erb
 
+set :public_folder, Proc.new { File.join(root, "..", "public") }
 
 
 
